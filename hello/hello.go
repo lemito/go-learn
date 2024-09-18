3bdde1b423c51e35ab2810e0b7b9d748e4746672
Michael lemito <delovoy.lemit@gmail.com>
2024-09-18 14:02:45 +0300

    first commit

---
+++ hello/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string){
+	return y, x
+}
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println(time.Now())
+	fmt.Printf("Rand float is = %f\n", rand.Float64())
+	fmt.Printf("PI is = %f\n", math.Pi)
+	fmt.Printf("%d + %d = %d\n", 5, 3, add(5, 3))
+	a, b := swap("a", "b")
+	fmt.Println(a, " ", b);
+}
