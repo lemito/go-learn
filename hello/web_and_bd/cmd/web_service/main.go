7a4eb94fb5a54d5687ada271e9b40531bb7f8d12
Michael lemito <delovoy.lemit@gmail.com>
2025-05-22 00:03:46 +0300

    попытки в веб

--- hello/web_and_bd/cmd/web_service/main.go
+++ hello/web_and_bd/cmd/web_service/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
-	// "github.com/lemito/web_and_bd/internal/db"
+	"github.com/lemito/web_and_bd/internal/db"
 )
 
 func main() {
-	// test_db, err := db.CreateDb("meow.db")
+	test_db, err := db.CreateDb("meow.db")
+	if err != nil {
+		test_db.Close()
+		panic(err)
+	}
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Pong!!")
-    })
+		fmt.Fprintf(w, "Pong!!")
+	})
+
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+
+		id_str := r.URL.Query().Get("id")
+		if id_str == "" {
+			http.Error(w, "", http.StatusBadRequest)
+		}
+
+		id, err := strconv.Atoi(id_str)
+		if err != nil {
+			http.Error(w, "", http.StatusBadRequest)
+		}
+
+		res, err := test_db.Get_from_id(uint32(id))
+		if err != nil {
+			http.Error(w, "", http.StatusBadRequest)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(res)
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
