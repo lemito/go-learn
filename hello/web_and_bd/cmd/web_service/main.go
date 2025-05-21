87c46a8d2dd531e81274eaa4b9b460e4d0973231
Michael lemito <delovoy.lemit@gmail.com>
2025-05-21 22:54:38 +0300

    web

--- hello/web_and_bd/cmd/web_service/main.go
+++ hello/web_and_bd/cmd/web_service/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"github.com/lemito/web_and_bd/internal/db"
+	"fmt"
+	"log"
+	"net/http"
+
+	// "github.com/lemito/web_and_bd/internal/db"
 )
 
 func main() {
-	test_db, err := db.CreateDb("meow.db")
+	// test_db, err := db.CreateDb("meow.db")
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Pong!!")
+    })
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
