99cd02d446ae66d9f8f86b7ff38d283fef9806c0
Michael lemito <delovoy.lemit@gmail.com>
2025-05-22 23:53:41 +0300

    add demo User

--- hello/web_and_bd/cmd/web_service/main.go
+++ hello/web_and_bd/cmd/web_service/main.go
@@ -11,9 +11,22 @@ import (
 )
 
 func main() {
+	log.Print("Start")
 	test_db, err := db.CreateDb("meow.db")
 	if err != nil {
 		test_db.Close()
+		panic(err)
+	}
+
+	defer test_db.Close()
+
+	test := db.User{
+		Id:   0,
+		Name: "Meow",
+	}
+
+	err = test_db.Insert(test)
+	if err != nil {
 		panic(err)
 	}
 
@@ -26,16 +39,20 @@ func main() {
 		id_str := r.URL.Query().Get("id")
 		if id_str == "" {
 			http.Error(w, "", http.StatusBadRequest)
+			return
+
 		}
 
 		id, err := strconv.Atoi(id_str)
 		if err != nil {
 			http.Error(w, "", http.StatusBadRequest)
+			return
 		}
 
 		res, err := test_db.Get_from_id(uint32(id))
 		if err != nil {
 			http.Error(w, "", http.StatusBadRequest)
+			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
