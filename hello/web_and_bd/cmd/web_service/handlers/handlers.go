96a3df80a97a5c35102419d994735a16dbe3dd19
Michael lemito <delovoy.lemit@gmail.com>
2025-05-24 23:53:24 +0300

    красота

--- hello/web_and_bd/cmd/web_service/handlers/handlers.go
+++ hello/web_and_bd/cmd/web_service/handlers/handlers.go
@@ -1,10 +1,80 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/lemito/web_and_bd/internal/db"
 )
+
+type Handler struct {
+	db *db.Database
+}
+
+func CreateHandler(db *db.Database) *Handler {
+	return &Handler{db: db}
+}
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Pong!!")
+}
+
+func (h *Handler) GetByIdHandler(w http.ResponseWriter, r *http.Request) {
+	id_str := r.URL.Query().Get("id")
+	if id_str == "" {
+		http.Error(w, "No id", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(id_str)
+	if err != nil {
+		http.Error(w, "Bad id", http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.db.Get_from_id(uint32(id))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("No such id or other err: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *Handler) GetAllHandler(w http.ResponseWriter, r *http.Request) {
+	res, err := h.db.Get_all()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *Handler) AddElemHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, `
+        	<form method="%s">
+                <input type="text" name="name" placeholder="Name">
+                <button type="submit">мяу</button>
+            </form>
+        `, http.MethodPost)
+	} else if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "Error: %v", err)
+			return
+		}
+
+		name := r.FormValue("name")
+		var usr db.User
+		usr.Name = name
+		h.db.Insert(usr)
+		fmt.Fprintf(w, "Added %s!", name)
+	}
 }
--- hello/web_and_bd/cmd/web_service/main.go
+++ hello/web_and_bd/cmd/web_service/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/lemito/web_and_bd/cmd/web_service/handlers"
 	"github.com/lemito/web_and_bd/internal/db"
@@ -21,66 +18,12 @@ func main() {
 
 	defer test_db.Close()
 
+	h := handlers.CreateHandler(test_db)
+
 	http.HandleFunc("/ping", handlers.PingHandler)
-
-	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
-
-		id_str := r.URL.Query().Get("id")
-		if id_str == "" {
-			http.Error(w, "No id", http.StatusBadRequest)
-			return
-		}
-
-		id, err := strconv.Atoi(id_str)
-		if err != nil {
-			http.Error(w, "Bad id", http.StatusBadRequest)
-			return
-		}
-
-		res, err := test_db.Get_from_id(uint32(id))
-		if err != nil {
-			http.Error(w, fmt.Sprintf("No such id or other err: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(res)
-	})
-
-	http.HandleFunc("/get_all", func(w http.ResponseWriter, r *http.Request) {
-		res, err := test_db.Get_all()
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(res)
-	})
-
-	http.HandleFunc("/add_elem", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			w.Header().Set("Content-Type", "text/html")
-			fmt.Fprintf(w, `
-        	<form method="%s">
-                <input type="text" name="name" placeholder="Name">
-                <button type="submit">мяу</button>
-            </form>
-        `, http.MethodPost)
-		} else if r.Method == http.MethodPost {
-			err := r.ParseForm()
-			if err != nil {
-				fmt.Fprintf(w, "Error: %v", err)
-				return
-			}
-
-			name := r.FormValue("name")
-			var usr db.User
-			usr.Name = name
-			test_db.Insert(usr)
-			fmt.Fprintf(w, "Added %s!", name)
-		}
-	})
+	http.HandleFunc("/get", h.GetByIdHandler)
+	http.HandleFunc("/get_all", h.GetAllHandler)
+	http.HandleFunc("/add_elem", h.AddElemHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
